6aeda854e3e66516ea4e7b13709e6d1b3833e8b9
Kulbhushan Mayer <kulbhushan.mayer@gmail.com>
2022-05-12 15:42:34 +0530

    updated

--- main.tf
+++ main.tf
@@ -19,15 +19,15 @@ resource "aws_security_group" "aws_security_group" {
   dynamic "ingress"{
     for_each = var.ingress
     content {
-      cidr_blocks = lookup(ingress.value, cidr_blocks, null)
-      description = lookup(ingress.value, description, null)
-      from_port = lookup(ingress.value, from_port, 0)
-      ipv6_cidr_blocks = lookup(ingress.value, ipv6_cidr_blocks, null)
-      prefix_list_ids = lookup(ingress.value, prefix_list_ids, null)
-      protocol = lookup(ingress.value, protocol, -1)
-      security_groups = lookup(ingress.value, security_groups, null)
-      self = lookup(ingress.value, self, null)
-      to_port = lookup(ingress.value, to_port, null)
+      cidr_blocks = lookup(ingress.value, "cidr_blocks", null)
+      description = lookup(ingress.value, "description", null)
+      from_port = lookup(ingress.value, "from_port", 0)
+      ipv6_cidr_blocks = lookup(ingress.value, "ipv6_cidr_blocks", null)
+      prefix_list_ids = lookup(ingress.value, "prefix_list_ids", null)
+      protocol = lookup(ingress.value, "protocol", -1)
+      security_groups = lookup(ingress.value, "security_groups", null)
+      self = lookup(ingress.value, "self", null)
+      to_port = lookup(ingress.value, "to_port", null)
     }
   }
 }
